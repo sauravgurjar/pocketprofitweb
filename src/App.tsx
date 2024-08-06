b82d4e9b7f81cbd30c6787c5e0c0143f142dfd59
sauravgurjar <58848027+sauravgurjar@users.noreply.github.com>
2024-08-07 00:07:38 +0530

    chnages updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import './input.css';
-import {MarqueeDemo} from "./comp/marque";
-import Header from "./comp/header";
-import LandingPage from "./comp/mainfile";
-import WhyUsSection from "./comp/secondheader";
+
 import UnderMaintenancePage from "./comp/mintancesite/UnderMaintenance";
 // import Footer from "./comp/footer";
 
